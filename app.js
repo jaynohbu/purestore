eae5d91b8f6920027e04e3039bc8f5fe8326a92c
Jay Noh <jay.noh@edriving.com>
2020-05-31 23:53:57 -0700

    moving media to product

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ async function upload(req, res) {
     new_name = sku + `_${moment().unix()}.` + file.name;
   }
 
-  let folder = __dirname + '/public/static/product/';
+  let folder = __dirname + '/public/static/products/';
   let path = folder + new_name;
   fs.writeFile(path, file.data, function (err) {
     if (err) {
@@ -100,7 +100,7 @@ async function upload(req, res) {
     if (req.query.sku) sku = req.query.sku;
     res.status(200).json({
       sku: sku,
-      path: '/static/product/' + new_name
+      path: '/static/products/' + new_name
     });
     let local_path = __dirname + '/public';
     exec(` aws s3 sync ${local_path}  s3://${config.CONTENT_S3_BUCKET}/`, (error, stdout, stderr) => {
