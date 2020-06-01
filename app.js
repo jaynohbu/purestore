27957e9238d7feda71522b52cf684bbd1db9e639
Jay Noh <jay.noh@edriving.com>
2020-05-31 23:20:19 -0700

    adding file one by one

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ async function upload(req, res) {
       path: path
     });
     exec(`
-    aws s3 sync ${mediapath}  s3://${config.CONTENT_S3_BUCKET}/
+    aws s3 sync ${folder}  s3://${config.CONTENT_S3_BUCKET}/
     `, (error, stdout, stderr) => {
       if (error) {
         console.log(error.stack);
